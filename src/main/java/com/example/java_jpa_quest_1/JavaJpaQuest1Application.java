cec065a75b3fb3a5afc0ad36a66c246b922115ee
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-06-25 11:38:14 +0200

    quest init and configuration

---
+++ src/main/java/com/example/java_jpa_quest_1/JavaJpaQuest1Application.java
@@ -0,0 +1,13 @@
+package com.example.java_jpa_quest_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJpaQuest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJpaQuest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_jpa_quest_1/JavaJpaQuest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_jpa_quest_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJpaQuest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
